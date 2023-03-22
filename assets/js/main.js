76543449407a4f533ed2dd8712e65332c7819e85
gluke0 <milanesigianluca@gmail.com>
2023-03-22 16:41:21 +0100

    calcolo biglietto completo

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,11 +6,38 @@ va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
 - chiedere km percorso e età del passeggero
-
+    - prompt o input per km
+    - prompt o input per età
 - calcolo costo viaggio (1km = 0.21€)
-
+    - calc km * costo/km
 - sconto
-
+    - se passeggero min -> 20% off
+    - se passeggero over 65 -> 40% off
 - display prezzo con massimo 2 decimali 
-
+    - 2.433333 -> 2.43
 */
+
+// prompt
+const name = prompt('Inserisci il tuo nome');
+const surname = prompt('Inserisci il tuo cognome');
+const age = prompt('Inserisci la tua età');
+const distance = prompt('Inserisci KM totali itinerario');
+
+// costo
+rate = 0.21;
+standardPrice = distance * rate;
+
+// sconto
+off20 = standardPrice - (standardPrice * 0.20);
+off40 = standardPrice - (standardPrice * 0.40);
+
+if (age < 18){
+    total = off20;
+} else if (age >= 65){
+    total = off40
+} else {
+    total = standardPrice;
+}
+ 
+// print
+document.getElementById('price').innerHTML = total.toFixed(2);
